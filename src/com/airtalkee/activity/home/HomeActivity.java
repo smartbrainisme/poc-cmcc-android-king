616b925e9f42d02bfcefd6b5f00943047b24a477
chenqingfei <qingfei.chen@beastbikes.com>
2015-10-27 00:14:03 +0800

    记得每次 push之前要pull最新的代码，要不然会有冲突

--- src/com/airtalkee/activity/home/HomeActivity.java
+++ src/com/airtalkee/activity/home/HomeActivity.java
@@ -1,11 +1,6 @@
 package com.airtalkee.activity.home;
 
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
-import java.util.List;
-import android.content.Context;
->>>>>>> 953be99d96ed385cb38d51dabe14e951b1681af7
+
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -14,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
 import com.airtalkee.R;
 import com.airtalkee.Util.DensityUtil;
 import com.airtalkee.activity.home.widget.MediaStatusBar;
@@ -27,33 +23,9 @@ import com.airtalkee.widget.SlidingUpPanelLayout;
 import com.airtalkee.widget.SlidingUpPanelLayout.PanelSlideListener;
 import com.airtalkee.widget.SlidingUpPanelLayout.PanelState;
 
-<<<<<<< HEAD
 public class HomeActivity extends SessionDialogActivity implements PanelSlideListener,ViewChangeListener
 {
-	
-=======
-public class HomeActivity extends FragmentActivity implements
-		OnPageChangeListener, PanelSlideListener, ViewChangeListener
-{
 
-	private String TAG = "HOME_ACTIVITY";
-
-	public static final int PAGE_MEMBER = 0;
-	public static final int PAGE_PTT = 1;
-	public static final int PAGE_IM = 2;
-
-	private static final Class<?>[] TABS = {
-	/* 0 */MemberFragment.class,
-	/* 1 */PTTFragment.class,
-	/* 2 */IMFragment.class, };
-
-	private ViewPager viewPager;
-	private PageFragmentAdapter adapter;
-	private PageIndicator mPageIndicator;
-	private int pageIndex = PAGE_PTT;
-
-	private MediaStatusBar mediaStatusBar;
->>>>>>> 953be99d96ed385cb38d51dabe14e951b1681af7
 	private SlidingUpPanelLayout mLayout;
 	private ImageView slidingBack;
 	private SessionAndChannelView channelView;
@@ -68,18 +40,12 @@ public class HomeActivity extends FragmentActivity implements
 
 		setRequestedOrientation(Config.screenOrientation);
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
-<<<<<<< HEAD
 		
 		super.mediaStatusBar = (MediaStatusBar) findViewById(R.id.media_status_function_bar);
 		mediaStatusBar.init((StatusBarTitle) findViewById(R.id.media_status_title_bar),
 				AirSessionControl.getInstance().getCurrentChannelSession());
 		
-=======
 
-		mediaStatusBar = (MediaStatusBar) findViewById(R.id.media_status_function_bar);
-		mediaStatusBar.init((StatusBarTitle) findViewById(R.id.media_status_title_bar));
-
->>>>>>> 953be99d96ed385cb38d51dabe14e951b1681af7
 		final FragmentManager fm = getSupportFragmentManager();
 		this.viewPager = (ViewPager) findViewById(R.id.home_activity_page_content);  
 		this.adapter = new PageFragmentAdapter(this, fm);
@@ -100,43 +66,6 @@ public class HomeActivity extends FragmentActivity implements
 		contaner.addView(channelView);
 		slidingBack = (ImageView) channelView.findViewById(R.id.sliding_back);
 	}
-
-	@Override
-	public void onPanelSlide(View panel, float slideOffset)
-	{
-		Log.i(TAG, "onPanelSlide, offset " + slideOffset);
-	}
-
-	@Override
-	public void onPanelExpanded(View panel)
-	{
-		Log.i(TAG, "onPanelExpanded");
-		contaner.setBackgroundColor(0xff222222);
-		slidingBack.setVisibility(View.VISIBLE);
-		channelView.resume();
-	}
-
-	@Override
-	public void onPanelCollapsed(View panel)
-	{
-		Log.i(TAG, "onPanelCollapsed");
-		contaner.setBackgroundColor(0x00000000);
-		slidingBack.setVisibility(View.GONE);
-		this.onPageSelected(pageIndex);
-	}
-
-	@Override
-	public void onPanelAnchored(View panel)
-	{
-		Log.i(TAG, "onPanelAnchored");
-	}
-
-	@Override
-	public void onPanelHidden(View panel)
-	{
-		Log.i(TAG, "onPanelHidden");
-	}
-<<<<<<< HEAD
 	
 	 @Override
      public void onPanelSlide(View panel, float slideOffset) {
@@ -169,138 +98,6 @@ public class HomeActivity extends FragmentActivity implements
          Log.i("HOME_ACTIVITY", "onPanelHidden");
      }
 	
-=======
-
-	@Override
-	protected void onResumeFragments()
-	{
-		// TODO Auto-generated method stub
-		super.onResumeFragments();
-		this.onPageSelected(pageIndex);
-	}
-
-	@Override
-	public void onPageScrollStateChanged(int arg0)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onPageScrolled(int arg0, float arg1, int arg2)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onPageSelected(int page)
-	{
-		// TODO Auto-generated method stub
-		if (mediaStatusBar != null)
-			mediaStatusBar.onPageChanged(page);
-		if (mPageIndicator != null)
-			mPageIndicator.onPageChanged(page);
-
-		for (int i = 0; i < TABS.length; i++)
-		{
-			if (null != adapter)
-				if (i == page)
-				{
-					adapter.getItem(i).onResume();
-				}
-				else
-				{
-					adapter.getItem(i).onPause();
-				}
-		}
-		pageIndex = page;
-		viewPager.setCurrentItem(pageIndex);
-	}
-
-	@Override
-	protected void onResume()
-	{
-		// TODO Auto-generated method stub
-		super.onResume();
-	}
-
-	final class PageFragmentAdapter extends FragmentPagerAdapter
-	{
-		private final List<BaseFragment> fragments = new ArrayList<BaseFragment>();
-
-		public PageFragmentAdapter(Context ctx, FragmentManager fm)
-		{
-			super(fm);
-			for (int i = 0; i < TABS.length; i++)
-			{
-				this.fragments.add(BaseFragment.newInstantiate(ctx, TABS[i].getName(), mediaStatusBar));
-			}
-		}
-
-		@Override
-		public Fragment getItem(int position)
-		{
-			return this.fragments.get(position);
-		}
-
-		@Override
-		public int getCount()
-		{
-			return this.fragments.size();
-		}
-	}
-
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev)
-	{
-		// TODO Auto-generated method stub
-		if (ev.getAction() == MotionEvent.ACTION_DOWN)
-		{
-			// View v = getCurrentFocus();
-			View v = mediaStatusBar.getBottomBarParent();
-			if (isShouldHideInput(v, ev))
-			{
-
-				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-				if (imm != null)
-				{
-					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-				}
-			}
-			return super.dispatchTouchEvent(ev);
-		}
-
-		if (getWindow().superDispatchTouchEvent(ev))
-		{
-			return true;
-		}
-		return onTouchEvent(ev);
-	}
-
-	public boolean isShouldHideInput(View v, MotionEvent event)
-	{
-		if (v != null)
-		{
-			int[] leftTop = { 0, 0 };
-			v.getLocationInWindow(leftTop);
-			int left = leftTop[0];
-			int top = leftTop[1];
-			int bottom = top + v.getHeight();
-			int right = left + v.getWidth();
-			if (event.getX() > left && event.getX() < right && event.getY() > top && event.getY() < bottom)
-			{
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
->>>>>>> 953be99d96ed385cb38d51dabe14e951b1681af7
 	@Override
 	public void onViewChanged(String sessionCode)
 	{
