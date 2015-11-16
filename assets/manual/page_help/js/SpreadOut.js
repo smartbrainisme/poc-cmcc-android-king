f80421ecec032336133220bc5aac41dee705dc82
zcyMac <zuochengyao@haoliantech.com>
2015-11-16 17:15:06 +0800

    2015年11月16日17:15:03

--- assets/manual/page_help/js/SpreadOut.js
+++ assets/manual/page_help/js/SpreadOut.js
@@ -1,17 +1,11 @@
-//չ������Ч��
 function show(e, state) {
 	if (state == 1) {
-		// ƽ��չ��
 		$(e).animate({
 			height : "320px"
 		}, 600);
-		// �л�����
 		$(e).css("background-image", "url(img/box_bg_open_lager.png)");
-		// �л���ͷ����
 		$(e).find(".arrow").attr("src", "img/iconfont-zuo copy.png");
-		// ���¼���2���open
 		$(e).attr("onclick", "show(this,2)");
-		// ��ʾ����
 		$(e).find(".intro").fadeIn(500);
 
 	} else {
